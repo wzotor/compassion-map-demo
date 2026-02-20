00869ede1a722a5891e1214fca4362cbb29ee82b
Wisdom Zotor <zotorwisdom@gmail.com>
2026-02-20 00:41:03 -0500

    Add national map and admin dashboard improvements

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from centers import views as centers_views
 
+admin.site.site_header = "Compassion Map Admin"
+admin.site.site_title = "Compassion Map Admin"
+admin.site.index_title = "National Office Dashboard"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
 
