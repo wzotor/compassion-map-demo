dbdedc0bc6379ffa0ccb5147ec702e58926a1140
Wisdom Zotor <zotorwisdom@gmail.com>
2026-02-20 15:47:47 -0500

    Log admin create update delete actions

--- centers/admin.py
+++ centers/admin.py
@@ -1,15 +1,13 @@
 from django.contrib import admin
-from .models import ProjectCenter
+from .models import ProjectCenter, Participant, UserProfile, AuditLog
+
 
 @admin.register(ProjectCenter)
 class ProjectCenterAdmin(admin.ModelAdmin):
-    list_display = ('name', 'center_code', 'territory', 'cluster', 'beneficiaries')
-    search_fields = ('name', 'center_code')
-    list_filter = ('territory', 'cluster')
+    list_display = ("name", "center_code", "territory", "cluster", "beneficiaries")
+    search_fields = ("name", "center_code")
+    list_filter = ("territory", "cluster")
 
-
-# Register your models here.
-from .models import Participant
 
 @admin.register(Participant)
 class ParticipantAdmin(admin.ModelAdmin):
@@ -23,10 +21,44 @@ class ParticipantAdmin(admin.ModelAdmin):
     list_filter = ("project_center", "sex")
     search_fields = ("participant_name", "participant_id")
 
-from .models import UserProfile
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+
+        AuditLog.objects.create(
+            user=request.user,
+            action="UPDATE" if change else "CREATE",
+            project_center=obj.project_center,
+            participant_id=obj.participant_id,
+            details=("Updated via Admin" if change else "Created via Admin"),
+        )
+
+    def delete_model(self, request, obj):
+        AuditLog.objects.create(
+            user=request.user,
+            action="DELETE",
+            project_center=obj.project_center,
+            participant_id=obj.participant_id,
+            details="Deleted via Admin",
+        )
+        super().delete_model(request, obj)
+
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ("user", "project_center")
     search_fields = ("user__username", "user__email", "project_center__name")
     list_filter = ("project_center",)
+
+
+@admin.register(AuditLog)
+class AuditLogAdmin(admin.ModelAdmin):
+    list_display = ("timestamp", "action", "user", "project_center", "participant_id")
+    list_filter = ("action", "project_center", "timestamp")
+    search_fields = (
+        "participant_id",
+        "details",
+        "user__username",
+        "project_center__center_code",
+        "project_center__name",
+    )
+    ordering = ("-timestamp",)
